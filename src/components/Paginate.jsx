1a50cded996d0addfc25bd4d503257220f998252
Mehdi01-git <louihranim@gmail.com>
2023-02-02 21:39:34 +0100

    Injecting Data

--- src/components/Paginate.jsx
+++ src/components/Paginate.jsx
@@ -15,14 +15,13 @@ import Typography from "@mui/material/Typography";
 import Paper from "@mui/material/Paper";
 import absences from "../assets/absences.json";
 import members from "../assets/members.json";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Switch from "@mui/material/Switch";
 import { visuallyHidden } from "@mui/utils";
 
-function createData(name, type, period, memNote, status, admitNote) {
+function createData(key, name, type, period, memNote, status, admitNote) {
   return {
+    key,
     name,
     type,
     period,
@@ -32,27 +31,23 @@ function createData(name, type, period, memNote, status, admitNote) {
   };
 }
 
-// const newRows = [
-//   absences.payload.map((absence, i) => {
-//     return createData(absence.userId);
-//   }),
-// ];
-
-const rows = [
-  createData("Cupcake", 305, 3.7, 67, 4.3, 5),
-  createData("Donut", 452, 25.0, 51, 4.9),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Gingerbread", 356, 16.0, 49, 3.9),
-  createData("Honeycomb", 408, 3.2, 87, 6.5),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Jelly Bean", 375, 0.0, 94, 0.0),
-  createData("KitKat", 518, 26.0, 65, 7.0),
-  createData("Lollipop", 392, 0.2, 98, 0.0),
-  createData("Marshmallow", 318, 0, 81, 2.0),
-  createData("Nougat", 360, 19.0, 9, 37.0),
-  createData("Oreo", 437, 18.0, 63, 4.0),
-];
+const rows = absences.payload.map((absence, i) => {
+  return createData(
+    i,
+    members.payload.map((member) => {
+      return member.userId === absence.userId ? member.name : "";
+    }),
+    absence.type,
+    `from ${absence.startDate} to ${absence.endDate}`,
+    absence.memberNote === "" ? "-" : absence.memberNote,
+    absence.rejectedAt !== null
+      ? "Rejected"
+      : absence.confirmedAt !== null
+      ? "Confirmed"
+      : "Requested",
+    absence.admitterNote === "" ? "-" : absence.admitterNote
+  );
+});
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -126,17 +121,11 @@ const headCells = [
 ];
 
 function EnhancedTableHead(props) {
-  const {
-    onSelectAllClick,
-    order,
-    orderBy,
-    numSelected,
-    rowCount,
-    onRequestSort,
-  } = props;
+  const { order, orderBy, onRequestSort } = props;
   const createSortHandler = (property) => (event) => {
     onRequestSort(event, property);
   };
+  console.log(rows);
 
   return (
     <TableHead>
@@ -154,7 +143,7 @@ function EnhancedTableHead(props) {
               direction={orderBy === headCell.id ? order : "asc"}
               onClick={createSortHandler(headCell.id)}
             >
-              {headCell.label}
+              <b>{headCell.label}</b>
               {orderBy === headCell.id ? (
                 <Box component="span" sx={visuallyHidden}>
                   {order === "desc" ? "sorted descending" : "sorted ascending"}
@@ -205,7 +194,7 @@ function EnhancedTableToolbar(props) {
         </Typography>
       ) : (
         <Typography
-          sx={{ flex: "1 1 100%" }}
+          sx={{ flex: "1 1 100%", marginLeft: "35px" }}
           variant="h6"
           id="tableTitle"
           component="div"
@@ -226,8 +215,8 @@ export default function EnhancedTable() {
   const [orderBy, setOrderBy] = React.useState("type");
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
-  const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [dense, setDense] = React.useState(true);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === "asc";
@@ -242,26 +231,6 @@ export default function EnhancedTable() {
       return;
     }
     setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1)
-      );
-    }
-
-    setSelected(newSelected);
   };
 
   const handleChangePage = (event, newPage) => {
@@ -311,11 +280,10 @@ export default function EnhancedTable() {
                   return (
                     <TableRow
                       hover
-                      onClick={(event) => handleClick(event, row.name)}
                       role="checkbox"
                       aria-checked={isItemSelected}
                       tabIndex={-1}
-                      key={row.name}
+                      key={row.key}
                       selected={isItemSelected}
                     >
                       <TableCell padding="checkbox"></TableCell>
@@ -328,10 +296,14 @@ export default function EnhancedTable() {
                         {row.name}
                       </TableCell>
                       <TableCell align="right">{row.type}</TableCell>
-                      <TableCell align="right">{row.period}</TableCell>
+                      <TableCell style={{ width: "20%" }} align="right">
+                        {row.period}
+                      </TableCell>
                       <TableCell align="right">{row.memNote}</TableCell>
                       <TableCell align="right">{row.status}</TableCell>
-                      <TableCell align="right">{row.admitNote}</TableCell>
+                      <TableCell style={{ width: "20%" }} align="right">
+                        {row.admitNote}
+                      </TableCell>
                     </TableRow>
                   );
                 })}
